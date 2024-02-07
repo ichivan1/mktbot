eb6b38c627d0a434b6a4df2d51235358260cfe8b
ichivan1 <128284560+ichivan1@users.noreply.github.com>
2024-02-07 00:53:31 -0600

    Update buscarcp.js

--- buscarcp.js
+++ buscarcp.js
@@ -6,7 +6,7 @@ async function searchPostalCode(postalCode) {
     try {
         const data = await fs.readFile('codigos_postales_cdmx.json', 'utf8');
         const postalCodes = JSON.parse(data);
-        return postalCodes.find(pc => pc.postal_code === parseInt(postalCode, 10)) || null;
+        return postalCodes.find(pc => pc.postal_code === postalCode) || null;
     } catch (error) {
         console.error(`Error reading or parsing the file: ${error}`);
         return null;
@@ -16,9 +16,13 @@ async function searchPostalCode(postalCode) {
 // handleMessages.js
 
 async function handlePostalCodeLookup({ mensajeEntrante, numberWa, sock }) {
+  // respuesta cdmx
   let rpositiva ="El envío es express🛵(se te puede entregar de 24 a 48 horas al día siguiente de realizar el pedido)\n\nEl método de pago💰 disponible es\n\n✅ Tarjeta de crédito o débito  💳\n✅ Transferencia  📥🧾\n✅ Mercado pago o tiendas de \n      conveniencia  ( por ejemplo \n      Oxxo) 🤩\n✅ Efectivo al momento de la \n      entrega  😀";
+  // respuesta guadalajara
+  let rgdlpositiva = "El envío es express🛵(se te puede entregar al día siguiente de realizar el pedido)\n\nEl método de pago💰 disponible es\n\n✅ Tarjeta de crédito o débito  💳\n✅ Transferencia  📥🧾\n✅ Mercado pago o tiendas de \n      conveniencia  ( por ejemplo \n      Oxxo) 🤩\n✅ Efectivo al momento de la \n      entrega  😀";
+  
     const result = await searchPostalCode(mensajeEntrante);
-  console.log(result+"---"+ mensajeEntrante);
+  
     if (result) {
         // Enviar mensaje de éxito
         await sock.sendMessage(numberWa, {
@@ -30,8 +34,35 @@ async function handlePostalCodeLookup({ mensajeEntrante, numberWa, sock }) {
 
        sock.ev.emit("respuestaAutomaticaEnviada", { numberWa });
     } else {
+      const result2 = await searchPostalCodeGDL(mensajeEntrante);
+      if (result2){
+        // Enviar mensaje de éxito gdl
+          await sock.sendMessage(numberWa, {
+              text: rgdlpositiva
+          });
+        console.log(rgdlpositiva);
+
+        guardarConversacion({ persona: numberWa, tipoMensaje: "enviado", contenidoMensaje: rgdlpositiva });
+
+         sock.ev.emit("respuestaAutomaticaEnviada", { numberWa });
+        
+      }else{
         // Enviar mensaje de fallo
         console.log(`Postal code ${mensajeEntrante} not found.`);
+      }
+        
+    }
+}
+
+//funcion de busqueda gdl
+async function searchPostalCodeGDL(postalCode) {
+    try {
+        const data = await fs.readFile('codigos_guadalajara.json', 'utf8');
+        const postalCodes = JSON.parse(data);
+        return postalCodes.find(pc => pc.postal_code === postalCode) || null;
+    } catch (error) {
+        console.error(`Error reading or parsing the file: ${error}`);
+        return null;
     }
 }
 
